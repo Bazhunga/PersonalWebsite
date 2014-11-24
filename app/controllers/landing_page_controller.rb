90b6d01a1ecdeb208017ac9db82e688441159a29
Bazhunga <zhu.s.kevin@gmail.com>
2014-11-24 00:16:57 -0500

    About page completed plus pictures and styles

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,28 +1,37 @@
 class LandingPageController < ApplicationController
   def About
+    @nav_status = ["active","","","","",""]
   end
 
   def Projects
+    @nav_status = ["","active","","","",""]
   end
   
   def ProjectsSoftware
+    @nav_status = ["","active","","","",""]
   end
 
   def ProjectsHardware
+    @nav_status = ["","active","","","",""]
   end
 
   def ProjectsPhotos
+    @nav_status = ["","active","","","",""]
   end
 
   def Github
+    @nav_status = ["","","active","","",""]
   end
 
   def LinkedIn
+    @nav_status = ["","","","active","",""]
   end
 
   def Resume
+    @nav_status = ["","","","","active",""]
   end
 
   def Contact
+    @nav_status = ["","","","","","active"]
   end
 end
